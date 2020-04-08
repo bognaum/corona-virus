c0895693f63407d4a576aec2d28cb87534eb509c
Bognaum <bognaum@gmail.com>
2020-04-08 10:14:48 +0300

    Added table, delta sick and delta sicked.

--- chart_files/main-script.js
+++ chart_files/main-script.js
@@ -1,25 +1,48 @@
 var 
+	prevSick    = 0,
 	prevSicked  = 0,
-	sickedStr   = "";
+	sickedStr   = "",
+	d_sickStr   = "",
 	d_sickedStr = "";
 
 data.forEach((v,i,a) => {
 	if (i) {
 		sickedStr   += ", ";
+		d_sickStr   += ", ";
 		d_sickedStr += ", ";
 	}
 	let 
 		sicked   = v.sick - v.die - v.cured,
+		d_sick   = v.sick - prevSick,
 		d_sicked = sicked - prevSicked;
 
-	sickedStr   += `${i * 30} ${-sicked   / 10 * 2}`;
-	d_sickedStr += `${i * 30} ${-d_sicked / 10 * 2}`;
+	sickedStr   += `${(i * 30) + 15} ${-sicked   / 10 * 2}`;
+	d_sickStr   += `${(i * 30) + 15} ${-d_sick   / 10 * 2}`;
+	d_sickedStr += `${(i * 30) + 15} ${-d_sicked / 10 * 2}`;
 
+	let code = `
+		<tr>
+			<td>${i + 1}</td>
+			<td>${v.date}</td>
+			<td>${v.sick}</td>
+			<td>${v.sick - prevSick}</td>
+			<td>${v.die}</td>
+			<td>${v.cured}</td>
+			<td>${v.sick - v.die - v.cured}</td>
+			<td>${v.sick - v.die - v.cured - prevSicked}</td>
+		</tr>
+	`;
+
+	tBody.innerHTML += code;
+
+	prevSick = v.sick;
 	prevSicked = sicked;
 });
 
 var code = `
 	<polyline points="${sickedStr  }" stroke="#00f" stroke-width="2" fill="none"/>
+	<polyline points="${d_sickStr  }" stroke="#0f0" stroke-width="2" fill="none"/>
 	<polyline points="${d_sickedStr}" stroke="#f00" stroke-width="2" fill="none"/>
 `;
+
 chart.innerHTML = code;
