b4d132df05f9e5ef90b79293c0105a15d68371fc
Bognaum <bognaum@gmail.com>
2020-04-28 10:35:17 +0300

    +data

--- chart_files/chart-data.js
+++ chart_files/chart-data.js
@@ -43,5 +43,6 @@ var data = [
 	{date: "2020.04.25", sick: 8125, die: 201 , cured: 782 , tested: 83577},
 	{date: "2020.04.26", sick: 8617, die: 209 , cured: 840 , tested: 89373},
 	{date: "2020.04.27", sick: 9009, die: 220 , cured: 864 , tested: 93519},
+	{date: "2020.04.28", sick: 9410, die: 239 , cured: 992 , tested: 98719},
 
 ];
